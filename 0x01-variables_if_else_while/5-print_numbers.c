00d1d3055d3a8c17fc6f0b242d74fbe01046e8f0
donni7 <yordanosbirhanu141@gmail.com>
2022-12-20 03:23:10 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * print numbers of base 10 staritn from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
