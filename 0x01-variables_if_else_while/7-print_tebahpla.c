be3e3a87552c16f16404318704453912b5e0371f
donni7 <yordanosbirhanu141@gmail.com>
2022-12-20 03:37:14 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * print alphabet reverse
+ * Retrun: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
