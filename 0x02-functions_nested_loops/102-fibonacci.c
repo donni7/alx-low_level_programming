0aee1b4e8fc3c5ac72fc3e80f1010ea6799f6818
donni7 <yordanosbirhanu141@gmail.com>
2022-12-20 13:36:25 -0800

    fibonacci 1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	long int fibonacci[50];
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%1d, %1d, ", fibonacci[0], fibonacci[1]);
+
+	for (i = 2; i < 50; i++)
+	{
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		if (i == 49)
+			printf("%1d\n", fibonacci[ii]);
+		else
+			printf("%1d, ", fibonacci[i]);
+	}
+	return (0);
+}
