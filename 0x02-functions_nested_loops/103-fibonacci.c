9861b59669429c1aa77d7580721a3fefff568153
donni7 <yordanosbirhanu141@gmail.com>
2022-12-20 13:51:44 -0800

    third fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print first fifty fibonacci
+ * Return: 0
+ */
+int main(void)
+{
+	long int i, x = 1, y = 2, sum = 0, tsum = 0;
+
+	for (i = 0; i < 49; i++)
+	{
+		if ((y % 2 == 0) && (y <= 4000000))
+		{
+			tsum = tsum + y;
+		}
+		sum = x + y;
+		x = y;
+		y = sum;
+	}
+	printf("%1d\n", tsum);
+	return (0);
+}
