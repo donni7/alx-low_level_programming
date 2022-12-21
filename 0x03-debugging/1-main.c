aca7c4f8daf1bc4e46578e6725c7697325cff1bd
donni7 <yordanosbirhanu141@gmail.com>
2022-12-20 16:39:31 -0800

    main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop :(\n");
+	i = 0;
+
+	/*
+	 * while (i < 10)
+	 * {
+	 *   putchar(i);
+	 *   }
+	 */
+	printf("Infinite loop avoided \\o/\n");
+	return (0);
+}
