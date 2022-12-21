aea89fbfa0c132d0fbf93c51aa3631fac084a8a8
donni7 <yordanosbirhanu141@gmail.com>
2022-12-21 04:56:17 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints alll numbers from
+ * 0 t0 9
+ * Return: returns nothing
+ */
+void print_numbers(void)
+{
+	int number = 48;
+
+	for (number = 48; number < 58; number++)
+	{
+		_putchar(number);
+	}
+	_putchar(10);
+}
