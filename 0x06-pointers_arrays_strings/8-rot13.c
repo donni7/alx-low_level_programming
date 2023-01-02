3d61f728df30ea54d8754a5f023dc40eaa00dc19
donni7 <yordanosbirhanu141@gmail.com>
2023-01-01 16:22:54 -0800

    rot

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rot13 - reverse array
+ * @str: s is array
+ * Return: 0
+ */
+char *rot13(char *str)
+{
+	int i = 0, k;
+	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (; str[i] != '\0'; i++)
+	{
+		for (k = 0; k <= 51; k++)
+		{
+			if (s[k] == str[i])
+			{
+				str[i] = s1[k];
+			}
+		}
+	}
+	return (str);
+}
