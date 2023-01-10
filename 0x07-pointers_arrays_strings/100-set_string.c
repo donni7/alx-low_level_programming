3d1fc11e36f7f49821cc587fdbe55326f1f38a4c
donni7 <yordanosbirhanu141@gmail.com>
2023-01-10 05:48:13 -0800

    advanced task 1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets valuse of pointer
+ * @s: pointer to pointer
+ * @to: pointer to char
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
