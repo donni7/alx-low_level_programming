aa355cd3cb5c4dfd04b800aafceb0f8f595ef5cc
donni7 <yordanosbirhanu141@gmail.com>
2023-01-12 02:44:00 -0800

    first task

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,6 @@
 void free_grid(int **grid, int height);
 char *str_concat(char *s1, char *s2);
-char *strdup(char *str);
+char *_strdup(char *str);
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 int **alloc_grid(int width, int height);
