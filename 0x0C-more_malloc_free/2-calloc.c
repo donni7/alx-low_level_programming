14dcf7f36b86710842f4b5fcf293beb684c6f5b9
donni7 <yordanosbirhanu141@gmail.com>
2023-01-12 13:15:31 -0800

    second task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - allocates memory
+ * @nmemb: number of elemetnts
+ * @size: seze of elements
+ * Return: NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*((char *)(p) + i) = 0;
+	}
+	
+	return (p);
+}
+
